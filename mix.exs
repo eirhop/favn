c63bf4149674ac2de8dcf94acead893563517f82
eirhop <36072925+eirhop@users.noreply.github.com>
2026-09-22 11:28:52 +0200

    Prepare Favn 0.5.0-rc.16

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule FavnUmbrella.MixProject do
         :favn_test_support,
         :favn_view
       ],
-      version: "0.5.0-rc.15",
+      version: "0.5.0-rc.16",
       elixir: "~> 1.20",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
